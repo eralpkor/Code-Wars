1917f310dd926e9892cedce7784aec42cdd6ec5f
eralpkor <eralp.ny@gmail.com>
2021-10-27 17:52:47 -0400

    Add sol. for phone nums

--- CodeWars-Solutions-6kyu/CreatePhone.js
+++ CodeWars-Solutions-6kyu/CreatePhone.js
@@ -14,6 +14,8 @@ function createPhoneNumber(numbers){
 }
 
 // regex
-function createPhoneNumber(numbers){
+function createPhoneNumberR(numbers){
   return numbers.join('').replace(/(\d{3})(\d{3})(\d{4})/,'($1) $2-$3');
 }
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // "(123) 456-7890");
